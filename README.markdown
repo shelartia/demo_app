bd5fe142408e56a4b7e66206823e3907e3d2056a
shelartia <artemshelepun@gmail.com>
2013-09-23 10:38:08 +0300

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,5 @@
+﻿# Ruby on Rails Tutorial: первое приложение
+
+Это первое приложение для
+[*Ruby on Rails Tutorial: Изучение Rails*](http://railstutorial.org/)
+ [shelartia](http://).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'd850918adec28b103dab9d38953839982b863dded49f6a946a30c04024cdd855db8615cb691ac6219871e72157ba26c8d01c1d74625277da3447a324749941e7'
